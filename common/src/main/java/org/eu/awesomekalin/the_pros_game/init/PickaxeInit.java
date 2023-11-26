ea2788da9715288b934474c8ed1d046175867e3b
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-11-26 11:47:10 +0000

    Redstone Pickaxe and preparation for v2.0-alpha.1

--- common/src/main/java/org/eu/awesomekalin/the_pros_game/init/PickaxeInit.java
+++ common/src/main/java/org/eu/awesomekalin/the_pros_game/init/PickaxeInit.java
@@ -5,10 +5,7 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
 import org.eu.awesomekalin.the_pros_game.ThePro_sGame;
-import org.eu.awesomekalin.the_pros_game.pickaxes.EmeraldPickaxe;
-import org.eu.awesomekalin.the_pros_game.pickaxes.GlowstonePickaxe;
-import org.eu.awesomekalin.the_pros_game.pickaxes.LapisPickaxe;
-import org.eu.awesomekalin.the_pros_game.pickaxes.ObsidianPickaxe;
+import org.eu.awesomekalin.the_pros_game.pickaxes.*;
 
 public class PickaxeInit {
     public static DeferredRegister<Item> PICKAXES = DeferredRegister.create(ThePro_sGame.MOD_ID, Registries.ITEM);
@@ -17,4 +14,5 @@ public class PickaxeInit {
     public static RegistrySupplier<Item> GLOWSTONE_PICKAXE= PICKAXES.register("glowstone_pickaxe", GlowstonePickaxe::new);
     public static RegistrySupplier<Item> LAPIS_PICKAXE = PICKAXES.register("lapis_pickaxe", LapisPickaxe::new);
     public static RegistrySupplier<Item> OBSIDIAN_PICKAXE = PICKAXES.register("obsidian_pickaxe", ObsidianPickaxe::new);
+    public static RegistrySupplier<Item> REDSTONE_PICKAXE = PICKAXES.register("redstone_pickaxe", RedstonePickaxe::new);
 }
