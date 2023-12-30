8d171de6fd1ff322d20cd168cd1fff5c58153b1b
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-12-30 12:55:47 +0000

    Redstone Axe

--- common/src/main/java/org/eu/awesomekalin/the_pros_game/init/AxeInit.java
+++ common/src/main/java/org/eu/awesomekalin/the_pros_game/init/AxeInit.java
@@ -5,10 +5,7 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
 import org.eu.awesomekalin.the_pros_game.ThePro_sGame;
-import org.eu.awesomekalin.the_pros_game.axes.EmeraldAxe;
-import org.eu.awesomekalin.the_pros_game.axes.GlowstoneAxe;
-import org.eu.awesomekalin.the_pros_game.axes.LapisAxe;
-import org.eu.awesomekalin.the_pros_game.axes.ObsidianAxe;
+import org.eu.awesomekalin.the_pros_game.axes.*;
 
 public class AxeInit {
     public static DeferredRegister<Item> AXES = DeferredRegister.create(ThePro_sGame.MOD_ID, Registries.ITEM);
@@ -17,4 +14,5 @@ public class AxeInit {
     public static RegistrySupplier<Item> GLOWSTONE_AXE = AXES.register("glowstone_axe", GlowstoneAxe::new);
     public static RegistrySupplier<Item> LAPIS_AXE = AXES.register("lapis_axe", LapisAxe::new);
     public static RegistrySupplier<Item> OBSIDIAN_AXE = AXES.register("obsidian_axe", ObsidianAxe::new);
+    public static RegistrySupplier<Item> REDSTONE_AXE = AXES.register("redstone_axe", RedstoneAxe::new);
 }
