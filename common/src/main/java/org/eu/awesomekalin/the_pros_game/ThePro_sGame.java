8c7fe66d5beaa32b0448d14dc72895f52ae13439
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-02-25 17:03:30 +0000

    Pro Food and Bump Version

--- common/src/main/java/org/eu/awesomekalin/the_pros_game/ThePro_sGame.java
+++ common/src/main/java/org/eu/awesomekalin/the_pros_game/ThePro_sGame.java
@@ -1,6 +1,7 @@
 package org.eu.awesomekalin.the_pros_game;
 
 import org.eu.awesomekalin.the_pros_game.init.AxeInit;
+import org.eu.awesomekalin.the_pros_game.init.FoodInit;
 import org.eu.awesomekalin.the_pros_game.init.PickaxeInit;
 import org.eu.awesomekalin.the_pros_game.init.SwordInit;
 
@@ -12,5 +13,6 @@ public class ThePro_sGame {
         PickaxeInit.PICKAXES.register();
         ProTab.TAB_REGISTER.register();
         AxeInit.AXES.register();
+        FoodInit.FOOD.register();
     }
 }
